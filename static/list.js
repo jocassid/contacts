9d7664c449703d95a70f02f7d654c748404ed41f
John Cassidy <jocassid@gmail.com>
2022-04-23 18:05:27 -0400

    Got edit and cancel buttons working

--- static/list.js
+++ static/list.js
@@ -1,12 +1,70 @@
 
+const fieldClasses = [
+    'first_name',
+    'last_name',
+    'mobile_phone',
+    'email'
+];
+
+function getFieldClass(textDivJQuery)
+{
+    let element = textDivJQuery[0];
+    for(const cssClass of element.classList)
+    {
+        if(fieldClasses.includes(cssClass))
+            return cssClass;
+    }
+    return null;
+}
+
+
+function getEventRow(event)
+{
+    return $(event.target).parents('tr');
+}
+
+
+function toggleRowButtons(row)
+{
+    let firstCell = row.find('td').first();
+    if(!firstCell)
+    {
+        console.error("Unable to find 1st cell");
+        return null;
+    }
+
+    firstCell.find('button.edit').toggle();
+    firstCell.find('button.save').toggle();
+    firstCell.find('button.cancel').toggle();
+}
+
+
 function setInputField(index, textDiv)
 {
-    console.log('setInputField');
     textDiv = $(textDiv);
+    let displayText = textDiv.text();
 
-    let input = textDiv.siblings('input');
+    let input = textDiv.siblings('input').first();
+    if(input.length === 1)
+    {
+        input.show();
+        input.val(displayText);
+        return;
+    }
 
-    console.log('foo');
+    let fieldClass = getFieldClass(textDiv);
+    if(!fieldClass)
+    {
+        console.error("no field class found");
+        return;
+    }
+
+    input = $(document.createElement('input'));
+    input.attr('name', 'fieldClass');
+    input.addClass('edit');
+    input.addClass(fieldClass);
+    textDiv.after(input);
+    input.val(displayText);
 }
 
 
@@ -18,13 +76,14 @@ function newClicked(event)
 
 function editClicked(event)
 {
-    console.log('editClicked');
-    let tr = $(event.target).parents('tr');
+    let tr = getEventRow(event);
 
     // hide the text used to display the value
     let display = tr.find("div.display");
     display.hide();
     display.each(setInputField);
+
+    toggleRowButtons(tr);
 }
 
 
@@ -36,7 +95,10 @@ function saveClicked(event)
 
 function cancelClicked(event)
 {
-    console.log('cancelClicked');
+    let tr = getEventRow(event);
+    toggleRowButtons(tr);
+    tr.find('div.display').show();
+    tr.find('input.edit').hide();
 }
 
 
