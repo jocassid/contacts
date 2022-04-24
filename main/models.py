d392b048236ef6bb51efe6c8a1f4b2563d7f4f32
snelzing <39928924+snelzing@users.noreply.github.com>
2022-04-24 08:52:05 -0400

    intial commit

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    mobile = models.CharField(max_length=13)
+    email = models.CharField(max_length=30)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.first_name
+
--- main/views.py
+++ main/views.py
@@ -1,8 +1,8 @@
-
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
+from .models import *
 
 
 class ListView(TemplateView):
@@ -14,25 +14,8 @@ class ListView(TemplateView):
         and then have the JavaScript perform a request to get a list of
         Contacts"""
 
-        return {
-            # replace this list of dicts with a queryset of
-            'contacts': [
-                {
-                    'pk': 1000000,
-                    'first_name': 'aaron',
-                    'last_name': 'aaronson',
-                    'mobile_phone': '614-555-0001',
-                    'email': 'aaaa@example.com'
-                },
-                {
-                    'pk': 1000001,
-                    'first_name': 'betty',
-                    'last_name': 'bailey',
-                    'mobile_phone': '614-555-0001',
-                    'email': 'betty.bailey@example.com',
-                },
-            ]
-        }
+        contacts = Contact.objects.__dict__
+        return contacts
 
 
 class ContactRestView(View):
